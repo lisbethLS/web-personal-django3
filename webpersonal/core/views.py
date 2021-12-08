12816fb6d13784582abc398d6ab75d77d2637d81
KlisbethLS <klopez5381@utm.edu.ec>
2021-12-08 10:07:06 -0500

    proyecto añadido webPersonal

---
+++ webpersonal/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+html_base= """
+      "<h1>Mi pagina virtual</h1>
+      <u1>
+          <li><a href="/">Portada</a></li>
+            <li><a href="/about/">Acerca de.....</a></li>
+            <li><a href="/folder/">Portafolio</a></li>
+            <li><a href="/contact/">contacto del grupo....</a></li>
+       <u1>  
+"""
+# Create your views here.
+
+def home(request):
+    return render(request,"core/home.html")
+def about(request):
+    return render(request,"core/about.html")
+def contact(request):
+   return render(request,"core/contact.html")
